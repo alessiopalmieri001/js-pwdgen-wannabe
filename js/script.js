329dc63d768cd9e9b410d7b7d724b1c65dd23f64
alessiopalmieri001 <alessiopalmieri001@gmail.com>
2023-11-30 16:34:00 +0100

    inizzializzazto file + prompt

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//inserimento username nel prompt
+let username_insert = prompt('inserisci qui il tuo nome');
+//debug per visualizzare il nome che ha dato l'utente
+console.log(username_insert);
+
+//inserimento surname nel prompt
+let surname_insert = prompt('inserisci qui il tuo cognome');
+//debug per visualizzare il cognome che ha dato l'utente
+console.log(surname_insert);
+
+//inserimento colore preferito nel prompt
+let color_insert = prompt('inserisci qui il tuo colore preferito')
+//debug per visualizzare il cognome che ha dato l'utente
+console.log(color_insert);
+
+let numero;
+numero = '114';
+console.log(numero);
+
+console.log(
+
+    `
+    ${username_insert}
+    ${surname_insert}
+    ${color_insert}
+    ${numero} 
+    `
+)
+    
