8ad61488c84cf0ad219688ecb9532c65a9e08657
alessiopalmieri001 <alessiopalmieri001@gmail.com>
2023-11-30 17:17:31 +0100

    prova con document .getelementbyid

--- js/script.js
+++ js/script.js
@@ -23,7 +23,8 @@ console.log(
     ${username_insert} ${surname_insert} ${color_insert} ${numero} 
     `
 )
-document.writeln(username_insert);
+/* document.writeln(username_insert);
 document.writeln(surname_insert);
 document.writeln(color_insert);
-document.writeln(numero);
+document.writeln(numero); */
+document.getElementById('pippo').innerHTML = `${username_insert} ${surname_insert} ${color_insert} ${numero} `;
